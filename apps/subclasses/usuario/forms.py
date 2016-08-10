3fb19ff15652cef3b582cff9fc04fa5eb6027f4b
luiz carlos <lcpojr@hotmail.com>
2016-08-10 13:22:14 -0300

    Subclasses initial 4

--- apps/subclasses/usuario/forms.py
+++ apps/subclasses/usuario/forms.py
@@ -13,5 +13,9 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 ##################################################
 #               CUSTOM IMPORTS                   #
 ##################################################
-
+from apps.default.forms import UserRegisterForm
 ##################################################
+
+class EmployeeRegisterForm(UserRegisterForm, forms.Form):
+
+	nickname = forms.CharField(label='Nickname:', max_length=20)
