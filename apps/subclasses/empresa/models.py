0f8ae107532d6bd3d46934653692e193527e8a1b
Luís Felipe <lfa.luisfelipe@gmail.com>
2016-08-12 14:10:37 -0300

    Subclasse empresa correções

--- apps/subclasses/empresa/models.py
+++ apps/subclasses/empresa/models.py
@@ -6,7 +6,7 @@ from apps.default.models import Empresa
 '''
 class Startup(models.Model):
 	id_startup = models.AutoField(primary_key=True)
-	empresa = models.OneToOneField(Empresa, on_delete = models.DO_NOTHING,related_name='startup_name')
+	empresa = models.OneToOneField(Empresa, on_delete = models.CASCADE,related_name='startup_name')
 	representante = models.CharField(max_length=150, default='', blank=True,null=True)
 	logo = models.ImageField(upload_to='media/subclasses/empresa/', blank=True,null=True, verbose_name='Imagem')
 	def __str__(self):
