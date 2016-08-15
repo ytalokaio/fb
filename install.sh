3ab3a29665d8b773260b8503c0e953a3fc039de1
Ytalo Martins <ytalo@3ysoftwarehouse.com.br>
2016-08-15 18:42:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ sudo apt-get -y install mysql-server
 pip3 install -r requirements.txt
 rm -r /etc/init/gunicorn.conf
 rm -r /etc/nginx/sites-available/django
-ln -s gunicorn.conf /etc/init/gunicorn.conf
-ln -s django /etc/nginx/sites-available/django
+ln -s /home/django/framework-base/gunicorn.conf /etc/init/gunicorn.conf
+ln -s /home/django/framework-base/django /etc/nginx/sites-available/django
 echo 'create database if not exists framework;' | mysql -u root -pframework
 python3 manage.py makemigrations
 python3 manage.py migrate
